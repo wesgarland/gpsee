4226935138a0e341cdfdc7253ae5975536a6942a
Donny Viszneki <hdon@kenai.com>
2009-05-21 13:29:11 -0400

    fixed memory leak + double free in ByteString finalizer

---
+++ gpsee_util.c
@@ -0,0 +1,187 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Initial Developer of the Original Code is PageMail, Inc.
+ *
+ * Portions created by the Initial Developer are 
+ * Copyright (c) 2007-2009, PageMail, Inc. All Rights Reserved.
+ *
+ * Contributor(s): 
+ * 
+ * Alternatively, the contents of this file may be used under the terms of
+ * either of the GNU General Public License Version 2 or later (the "GPL"),
+ * or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** 
+ */
+
+/**
+ *  @file	gpsee_util.c	General utility functions which have nothing
+ *				to do with GPSEE other than it uses them.
+ *  @author	Wes Garland, PageMail, Inc., wes@page.ca
+ *  @version	$Id: gpsee_util.c,v 1.3 2009/05/01 18:55:58 wes Exp $
+ *  @date	March 2009
+ */
+
+static const char __attribute__((unused)) rcsid[]="$Id: gpsee_util.c,v 1.3 2009/05/01 18:55:58 wes Exp $:";
+
+#include "gpsee.h"
+
+/** String copy function which insures NUL-terminated
+ *  dst string and does not zero-pad. Works only on
+ *  ASCIZ strings.
+ *
+ *  @param	dst		Memory to copy to, must be valid
+ *  @param	src		ASCIZ-String to copy from
+ *  @param	dst_size	Size of dst buffer.
+ *  @returns	pointer to dst's terminating NUL char
+ */
+char *gpsee_cpystrn(char *dst, const char *src, size_t dst_size)
+{
+  if (dst_size)
+  {
+    while (dst_size-- && (*dst++ = *src++));
+    *dst = (char)0;
+  }
+
+  return dst;
+}
+
+/** String concatenation function.
+ * 
+ *  Appends src to dst, without overrunning dst's buffer and
+ *  guaranteeing NUL termination.
+ *
+ *  @param dst		The string to append to
+ *  @param src		The string to append
+ *  @param dst_size	The size of dst's memory allocation
+ *  @returns		The number of unused bytes in dst, excluding the NLU
+ */
+size_t gpsee_catstrn(char *dst, const char *src, size_t dst_size)
+{
+  char *s;
+
+  if (!dst_size)
+    return 0;
+
+  for (s = dst; *s; s++);
+  dst_size -= (s - dst);
+  dst = s;
+
+  s = gpsee_cpystrn(dst, src, dst_size);
+  dst_size -= (s - dst);
+
+  return dst_size;
+}
+
+/** Returns a pointer to the filename portion of the passed argument.
+ *  @param	fullpath	Path to examine
+ *  @returns	Pointer inside fullpath to the base filename.
+ */
+const char *gpsee_basename(const char *fullpath)
+{
+  const char *slash = strrchr(fullpath, '/');
+
+  if (!slash)
+    return fullpath;
+
+  if (!slash[1])
+    return "/";
+
+  return slash + 1;
+}
+
+/** Returns the directory name component of the passed argument.
+ *  If the fullpath does not contain any slashes, it is assumed that
+ *  it is in the current directory, and "." is returned.
+ *
+ *  @param	fullpath	Path to examine
+ *  @param	buf		Buffer in which to copy by the name
+ *  @param	bufLen		Number of bytes allocated for buf
+ *  @returns	Pointer to buf, or NULL when buf is too small.
+ */
+const char *gpsee_dirname(const char *fullpath, char *buf, size_t bufLen)
+{
+  const char *slash = strrchr(fullpath, '/');
+
+  if (bufLen < 2)
+    return NULL;
+
+  if (!slash)
+  {
+    buf[0] = '.';
+    buf[1] = (char)0;
+    return buf;
+  }
+
+  if ((slash - fullpath) >= bufLen)
+    return NULL;
+
+  strncpy(buf, fullpath, slash - fullpath);
+  buf[slash - fullpath] = (char)0;
+
+  return buf;
+}
+
+int gpsee_resolvepath(const char *path, char *buf, size_t bufsiz)
+{
+#if defined(GPSEE_SUNOS_SYSTEM)
+  return resolvepath(path, buf, bufsiz);
+#else
+  char  *mBuf;
+  char  *rp;
+  int   ret;
+
+  errno = 0;
+
+  if (bufsiz < PATH_MAX)
+    mBuf = malloc(PATH_MAX);
+  else
+    mBuf = buf;
+
+  if (!mBuf)
+  {
+    if (!errno)
+      errno = EINVAL;
+    return -1;
+  }
+
+  rp = realpath(path, mBuf);
+  if (rp != buf)
+  {
+    char *s = gpsee_cpystrn(buf, rp, bufsiz);
+    if (s - buf == bufsiz) /* overrun */
+    {
+      errno = ENAMETOOLONG;
+      ret = -1;
+    }
+    else
+      ret = s - buf;
+  }
+  else
+    ret = strlen(buf);
+
+  if (mBuf != buf)
+    free(mBuf);
+
+  return ret;
+#endif
+}
---
+++ unix_rules.mk
@@ -0,0 +1,63 @@
+# Standard targets
+ifneq ($(MAKECMDGOALS),install-nodeps)
+ifneq ($(MAKECMDGOALS),clean)
+ifneq ($(MAKECMDGOALS),dist-clean)
+ifneq ($(MAKECMDGOALS),all-clean)
+ifneq ($(MAKECMDGOALS),build_debug)
+depend depend.mk: 	XFILES=$(sort $(filter %.c %.cc %.cpp %.cxx,$(DEPEND_FILES)))
+depend depend.mk: 	Makefile $(DEPEND_FILES)
+			@echo " * Building dependencies for: $(XFILES)"
+			$(if $(XFILES), $(MAKEDEPEND) $(MDFLAGS) $(CPPFLAGS) -DMAKEDEPEND $(XFILES) > $@)
+			@touch depend.mk
+endif # goal = build_debug
+endif # goal = all-clean
+endif # goal = dist-clean
+endif # goal = clean
+endif # goal = install-nodeps
+
+clean:
+	-$(if $(strip $(OBJS)), $(RM) $(OBJS))
+	-$(if $(strip $(EXPORT_LIBS)), $(RM) $(EXPORT_LIBS))
+	-$(if $(strip $(EXPORT_LIBEXEC_OBJS)), $(RM) $(EXPORT_LIBEXEC_OBJS)) 
+	-$(if $(strip $(PROGS)), $(RM) $(PROGS))
+	-$(if $(strip $(EXPORT_PROGS)), $(RM) $(EXPORT_PROGS))
+	-$(if $(strip $(AUTOGEN_HEADERS)), $(RM) $(AUTOGEN_HEADERS))
+	-$(if $(strip $(EXTRA_CLEAN_RULE)), $(MAKE) $(EXTRA_CLEAN_RULE))
+	$(RM) depend.mk
+
+build_debug:
+	@echo "Depend Files: $(DEPEND_FILES)"
+
+# Install shared libraries
+install-nodeps install install-solibs: XLIBS =$(strip $(filter %.$(SOLIB_EXT),$(EXPORT_LIBS)))
+install-solibs:	$(EXPORT_LIBS) $(EXPORT_LIBEXEC_OBJS)
+		@$(if $(XLIBS), [ -d $(SOLIB_DIR) ] || mkdir -p $(SOLIB_DIR))
+		$(if $(XLIBS), $(CP) $(XLIBS) $(SOLIB_DIR))
+		@$(if $(EXPORT_LIBEXEC_OBJS), [ -d $(LIBEXEC_DIR) ] || mkdir -p $(LIBEXEC_DIR))
+		$(if $(EXPORT_LIBEXEC_OBJS), $(CP) $(EXPORT_LIBEXEC_OBJS) $(LIBEXEC_DIR))
+
+# Install binaries and shared libraries
+install-nodeps install:	XPROGS =$(strip $(EXPORT_PROGS))
+install-nodeps install:	XCGIS =$(strip $(CGI_PROGS))
+install:	$(EXPORT_LIBS) $(EXPORT_LIBEXEC_OBJS) $(EXPORT_PROGS) $(CGI_PROGS)
+ifneq (X$(EXPORT_LIBS)$(EXPORT_LIBEXEC_OBJS),X)
+		@make install-solibs
+endif
+		@make install-nodeps
+
+install-nodeps:
+		@$(if $(XPROGS),[ -d $(BIN_DIR) ] || mkdir -p $(BIN_DIR))
+		$(if $(XPROGS), $(CP) $(EXPORT_PROGS) $(BIN_DIR))
+		@$(if $(XLIBS), [ -d $(SOLIB_DIR) ] || mkdir -p $(SOLIB_DIR))
+		$(if $(XLIBS), $(CP) $(XLIBS) $(SOLIB_DIR))
+		@$(if $(EXPORT_LIBEXEC_OBJS), [ -d $(LIBEXEC_DIR) ] || mkdir -p $(LIBEXEC_DIR))
+		$(if $(EXPORT_LIBEXEC_OBJS), $(CP) $(EXPORT_LIBEXEC_OBJS) $(LIBEXEC_DIR))
+
+# Propagate changes to headers and static libraries
+srcmaint:	XLIBS =$(strip $(filter %.$(LIB_EXT),$(EXPORT_LIBS)))
+srcmaint:	XHEADERS =$(strip $(EXPORT_HEADERS))
+srcmaint:	$(strip $(filter %.$(LIB_EXT),$(EXPORT_LIBS))) $(EXPORT_HEADERS)
+		@$(if $(XLIBS), [ -d $(STATICLIB_DIR) ] || mkdir -p $(STATICLIB_DIR))
+		$(if $(XLIBS), $(CP) $(XLIBS) $(STATICLIB_DIR))
+		@$(if $(XHEADERS), [ -d $(INCLUDE_DIR) ] || mkdir -p $(INCLUDE_DIR))
+		$(if $(XHEADERS), $(CP) $(XHEADERS) $(INCLUDE_DIR))
