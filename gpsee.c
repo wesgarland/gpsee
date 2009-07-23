8ebc52e3a90677451627a03ff9080c793f5fcd91
Wes Garland <wes@page.ca>
2009-07-23 14:46:46 -0400

    CVS rcsid sync

--- gpsee.c
+++ gpsee.c
@@ -37,7 +37,7 @@
  *  @file	gpsee.c 	Core GPSEE.
  *  @author	Wes Garland
  *  @date	Aug 2007
- *  @version	$Id: gpsee.c,v 1.8 2009/06/12 17:01:20 wes Exp $
+ *  @version	$Id: gpsee.c,v 1.9 2009/07/23 18:34:37 wes Exp $
  *
  *  Routines for running JavaScript programs, reporting errors via standard SureLynx
  *  mechanisms, throwing exceptions portably, etc. 
@@ -46,6 +46,9 @@
  *  standalone SureLynx JS shell. 
  *
  *  $Log: gpsee.c,v $
+ *  Revision 1.9  2009/07/23 18:34:37  wes
+ *  Added *gpsee_getInstancePrivateNTN, always set JS version
+ *
  *  Revision 1.8  2009/06/12 17:01:20  wes
  *  Merged with upstream
  *
@@ -90,7 +93,7 @@
  *
  */
 
-static __attribute__((unused)) const char gpsee_rcsid[]="$Id: gpsee.c,v 1.8 2009/06/12 17:01:20 wes Exp $";
+static __attribute__((unused)) const char gpsee_rcsid[]="$Id: gpsee.c,v 1.9 2009/07/23 18:34:37 wes Exp $";
 
 #define _GPSEE_INTERNALS
 #include "gpsee.h"
