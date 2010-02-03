d457a9ad7e69b111e86bd760a7bedab42eeab6ab
Donny Viszneki <hdon@page.ca>
2010-02-03 03:00:38 -0500

    core: added support for optional CommonJS Modules 1.1 feature "require.paths"; this and other changesets close issue 17

---
+++ tests/modules/paths/program.js
@@ -0,0 +1,13 @@
+var modname = 'test-module-2f2809gh8209hg2';
+var mod;
+try {
+  mod = require(modname)
+} catch (e) {
+  mod = false;
+  print('require() failed as it should :)');
+}
+if (mod)
+  throw new Error("require() should have failed! have you a module somewhere named \""+modname+"?\"");
+require.paths = ['directory'];
+mod = require(modname);
+mod.finishTest();
