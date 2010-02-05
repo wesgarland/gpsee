cb8a1deb4b2045461db59b6847a9c06ede0f0ceb
Wes Garland <wes@page.ca>
2010-02-05 16:32:59 -0500

    Added C Stack overflow protection facility to GPSEE-core

--- gsr.c
+++ gsr.c
@@ -37,7 +37,7 @@
  * @file	gsr.c		GPSEE Script Runner ("scripting host")
  * @author	Wes Garland
  * @date	Aug 27 2007
- * @version	$Id: gsr.c,v 1.18 2010/01/24 04:46:07 wes Exp $
+ * @version	$Id: gsr.c,v 1.19 2010/02/05 21:32:40 wes Exp $
  *
  * This program is designed to interpret a JavaScript program as much like
  * a shell script as possible.
@@ -54,7 +54,7 @@
  * is the usage() function.
  */
  
-static __attribute__((unused)) const char rcsid[]="$Id: gsr.c,v 1.18 2010/01/24 04:46:07 wes Exp $";
+static __attribute__((unused)) const char rcsid[]="$Id: gsr.c,v 1.19 2010/02/05 21:32:40 wes Exp $";
 
 #define PRODUCT_SHORTNAME	"gsr"
 #define PRODUCT_VERSION		"1.0-pre1"
@@ -558,6 +558,7 @@ PRIntn prmain(PRIntn argc, char **argv)
   }
 
   jsi = gpsee_createInterpreter(script_argv, script_environ);
+  gpsee_setThreadStackLimit(jsi->cx, &jsi);
   processFlags(jsi, flags);
   free(flags);
 
