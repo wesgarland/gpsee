fa6525b0bfd5a17a44806b2eb6e203e5555fee99
Nick Galbreath <nickgsuperstar@gmail.com>
2010-03-28 13:32:19 -0400

    fixups for scripts

--- modules/curl/easytest.js
+++ modules/curl/easytest.js
@@ -1,12 +1,23 @@
 
 const cURL = require('curl');
+const easycurl = cURL.easycurl;
+const easycurl_slist = cURL.easycurl_slist;
 const ByteArray = require('binary').ByteArray;
 
-var c = new cURL.Easy;
+
+var c = new easycurl;
+
+for (var i in c) {
+    print("" + i + ": " + c[i]);
+}
 
 var blob = new ByteArray();
 
-c.options.writefunction = function(data) {
+c.header = function(data) {
+    print("HEADER: " + data.trim());
+}
+
+c.write = function(data) {
     print("GOT: " + data.size);
 
     // can convert to a string here since data could be
@@ -16,11 +27,24 @@ c.options.writefunction = function(data) {
     blob.concat(ByteArray(data));
 }
 
+var headers = new easycurl_slist;
 
-c.options.url = 'http://www.google.com';
-c.options.verbose = 1;
+headers.append('foo: bar');
+headers.append('ding: bat');
+print("headers: " + headers.toArray());
+c.setopt(c.CURLOPT_HTTPHEADER, headers);
+
+c.setopt(c.CURLOPT_URL, 'http://www.google.com');
+
+// change this to "1" to show everything
+c.setopt(c.CURLOPT_VERBOSE, 0);
+
+c.setopt(c.CURLOPT_HTTPGET, 1);
 c.perform();
 
 // Now glue it all together
+print('-----------------------------------------')
 
 print(blob.decodeToString('utf8'));
+
+print("done");
