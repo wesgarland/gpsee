2f0776b5a73ea6eb2802f38379f81622884002a4
Wes Garland <wes@page.ca>
2010-04-01 10:14:59 -0400

    Exception handling: disabled source output when not a tty, added exit code test

---
+++ tests/error_reporting/exit_codes.sh
@@ -0,0 +1,29 @@
+#! /bin/sh
+
+[ "$GSR" ] || GSR=/usr/bin/gsr
+export GSR
+path="`dirname $0`"
+
+$GSR -F "$path/stack_dump.js" >/dev/null 2>&1
+exitCode="$?"
+if [ "$exitCode" = 1 ]; then
+  echo "OKAY: Exit code for an uncaught exception passes"
+else
+  echo "FAIL: Exit code for an uncaught exception was $exitCode"
+fi
+
+$GSR -F "$path/warning.js" >/dev/null 2>&1
+exitCode="$?"
+if [ "$exitCode" = 0 ]; then
+  echo "OKAY: Exit code for no error passes"
+else
+  echo "FAIL: Exit code for no error was $exitCode"
+fi
+
+$GSR -F "$path/syntax_error.js" >/dev/null 2>&1
+exitCode="$?"
+if [ "$exitCode" = 1 ]; then
+  echo "OKAY: Exit code for a syntax error passes"
+else
+  echo "FAIL: Exit code for a syntax error was $exitCode"
+fi
