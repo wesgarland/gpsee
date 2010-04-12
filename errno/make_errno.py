6a09db0770ddebe246418691aa34800cac92f8a3
Nick G <nickg@client9.com>
2010-04-11 22:26:57 -0400

    errno 4 gpsee

---
+++ errno/make_errno.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+errnos = [
+    'EPERM', 'ENOENT', 'ESRCH', 'EINTR', 'ENXIO', 'E2BIG', 'ENOEXEC', 'EBADF',
+    'ECHILD', 'EAGAIN', 'ENOMEM','EACCES', 'EFAULT', 'ENOTBLK', 'EBUSY',
+    'EEXIST', 'ENODEV', 'ENOTDIR', 'EISDIR', 'EINVAL', 'ENFILE', 'EMFILE',
+    'ENOTTY', 'ETXTBSY', 'EFBIG', 'ENOSPC', 'ESPIPE', 'EROFS', 'EMLINK',
+    'EPIPE', 'EDOM', 'ERANGE', 'EDEADLK', 'ENAMETOOLONG', 'ENOLCK', 'ENOSYS',
+    'ENOTEMPTY', 'EWOULDBLOCK', 'ENOMSG', 'EIDRM', 'ECHRNG', 'EL2NSYNC',
+    'EL3HLT', 'ELRST', 'ELNRNG', 'EUNATCH', 'ENOCSI', 'EL2HLT', 'EBADE',
+    'EBADR', 'EXFULL', 'ENOANO', 'EBADRQC', 'EBADSLT', 'EDEADLOCK', 'EBFONT',
+    'ENOSTR', 'ENODATA', 'ETIME', 'ENOSR', 'ENONET', 'ENOPKG', 'EREMOTE',
+    'ENOLINK', 'EADV', 'ESRMNT', 'ECOMM', 'EPROTO', 'EMULTIHOP', 'EDOTDOT',
+    'EBADMSG', 'EOVERFLOW', 'ENOTUNIQ', 'EBADFD', 'EREMCHG', 'ELIBACC',
+    'ELIBBAD', 'ELIBMAX', 'ELIBEXEC', 'EILSEQ', 'ERESTART', 'ESTRPIPE',
+    'EUSERS', 'ENOTSOCK', 'EDESTADDRREQ', 'EMSGSIZE',
+    'EPROTOTYPE','ENOPROTOOPT', 'ESOCKTNOSUPPORT', 'EOPNOTSUPP',
+    'EPFNOSUPPORT', 'EAFNOSUPPORT', 'EADDRINUSE','EADDRNOTAVAIL', 'ENETDOWN',
+    'ENETUNREACH', 'ENETRESET',
+    'ECONNABORTED', 'ECONNRESET', 'ENOBUFS', 'EISCONN', 'ENOTCONN',
+    'ESHUTDOWN',
+    'ETOOMANYREFS', 'ETIMEDOUT', 'ECONNREFUSED', 'EHOSTDOWN', 'EHOSTUNREACH',
+    'EALREADY', 'EINPROGRESS', 'ESTALE', 'EUCLEAN', 'ENOTNAME', 'ENAVAIL',
+    'EISNAME', 'EREMOTEIO', 'EDQUOT', 'ENOMEDIUM', 'EMEDIUMTYPE', 'ECANCELED',
+    'ENOKEY', 'EKEYEXPIRED', 'EKEYREVOKED', 'EKEYREJECTED',
+    'EOWNERDEAD', 'ENOTRECOVERABLE'
+    ]
+
+print "static JSConstDoubleSpec errno_constants[] = {"
+for e in errnos:
+    print """#ifdef %s
+{%s, "%s", 0,{0,0,0}},
+#endif""" % (e,e,e)
+print "{0,0,0,{0,0,0}}"
+print "};"
+
+print "static const char* errno2name(int e) {"
+print "switch (e) {"
+for e in errnos:
+    if e == 'EWOULDBLOCK':
+        # some platforms define aliases -- want only one
+        print "#if EWOULDBLOCK && (EWOULDBLOCK != EAGAIN)"
+    elif e == 'EDEADLOCK':
+        # some platforms define aliases -- want only one
+        print "#if EDEADLOCK && (EDEADLOCK != EDEADLK)"
+    else:
+        print "#ifdef %s" % e
+    print 'case %s: return "%s";\n#endif' % (e,e)
+print "default: return NULL;";
+print "}"  #end switch
+print "}"  # end function
