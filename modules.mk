c04514e0da323c9cfffec3fe077b3f749c36b5fa
Donny Viszneki <hdon@page.ca>
2010-05-11 16:18:08 -0400

    system: stdio properties of system module now work via fs-base.openDescriptor()

---
+++ modules.mk
@@ -0,0 +1,84 @@
+# ***** BEGIN LICENSE BLOCK *****
+# Version: MPL 1.1/GPL 2.0/LGPL 2.1
+#
+# The contents of this file are subject to the Mozilla Public License Version
+# 1.1 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+# http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS IS" basis,
+# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+# for the specific language governing rights and limitations under the
+# License.
+#
+# The Initial Developer of the Original Code is PageMail, Inc.
+#
+# Portions created by the Initial Developer are 
+# Copyright (c) 2007-2009, PageMail, Inc. All Rights Reserved.
+#
+# Contributor(s):
+# 
+# Alternatively, the contents of this file may be used under the terms of
+# either of the GNU General Public License Version 2 or later (the "GPL"),
+# or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+# in which case the provisions of the GPL or the LGPL are applicable instead
+# of those above. If you wish to allow use of your version of this file only
+# under the terms of either the GPL or the LGPL, and not to allow others to
+# use your version of this file under the terms of the MPL, indicate your
+# decision by deleting the provisions above and replace them with the notice
+# and other provisions required by the GPL or the LGPL. If you do not delete
+# the provisions above, a recipient may use your version of this file under
+# the terms of any one of the MPL, the GPL or the LGPL.
+#
+# ***** END LICENSE BLOCK ***** 
+#
+#
+# Generic Makefile for building a module.
+# Can be augmented with module.mk in the module's directory.
+#
+
+# MODULE holds the name of the module, eg. "binary", based on the
+# name of the current directory unless overrideen
+MODULE ?= $(notdir $(abspath .))
+MODULE := $(MODULE)
+
+# MODULE_OBJ is the object file containing the module. Additional
+# objects can be specified with EXTRA_MODULE_OBJS
+MODULE_OBJ ?= $(MODULE).o
+
+include $(GPSEE_SRC_DIR)/local_config.mk
+include $(GPSEE_SRC_DIR)/system_detect.mk
+include $(GPSEE_SRC_DIR)/version.mk
+-include $(GPSEE_SRC_DIR)/$(UNAME_SYSTEM)_config.mk
+include $(GPSEE_SRC_DIR)/$(STREAM)_stream.mk
+ifneq ($(MAKECMDGOALS),clean)
+include $(GPSEE_SRC_DIR)/spidermonkey/vars.mk
+endif
+-include module.mk
+include $(GPSEE_SRC_DIR)/build.mk
+ifneq ($(MAKECMDGOALS),depend)
+-include depend.mk
+endif
+
+CPPFLAGS	:= -I$(GPSEE_SRC_DIR) $(CPPFLAGS)
+
+DEPEND_FILES += $(GPSEE_SRC_DIR)/modules.mk module.mk 
+DEPEND_FILES += $(wildcard $(MODULE_OBJ:.o=.c) $(MODULE_OBJ:.o=.cpp))
+DEPEND_FILES += $(wildcard $(EXTRA_MODULE_OBJS:.o=.c) $(EXTRA_MODULE_OBJS:.o=.cpp))
+
+ifdef VERSION_O
+ifneq (X$(wildcard $(VERSION_H)),X)
+EXTRA_MODULE_OBJS += $(VERSION_O)
+else
+VERSION_O := 
+endif
+endif
+
+$(MODULE).$(LIB_EXT) $(MODULE).$(SOLIB_EXT): $(MODULE_OBJ) $(EXTRA_MODULE_OBJS)
+
+.PHONY: build_debug_module
+
+clean: OBJS += $(MODULE_OBJ) $(EXTRA_MODULE_OBJS) $(MODULE).$(LIB_EXT) $(MODULE).$(SOLIB_EXT) $(wildcard $(PROGS:=.o))
+
+
+
---
+++ modules/system/system.c
@@ -0,0 +1,175 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Initial Developer of the Original Code is PageMail, Inc.
+ *
+ * Portions created by the Initial Developer are 
+ * Copyright (c) 2007-2009, PageMail, Inc. All Rights Reserved.
+ *
+ * Contributor(s): 
+ * 
+ * Alternatively, the contents of this file may be used under the terms of
+ * either of the GNU General Public License Version 2 or later (the "GPL"),
+ * or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** 
+ */
+
+/**
+ *  @file       system.c	GPSEE implementation of the CommonJS "system"
+ *                              module with lazily loaded module dependencies
+ *                              for the stdio features.
+ *  @author     Donny Viszneki
+ *  @date       Feb 2010
+ *  @version    $Id: system.c,v 1.2 2010/03/10 16:26:42 wes Exp $
+ */
+ 
+static __attribute__((unused)) const char rcsid[]="$Id: system.c,v 1.2 2010/03/10 16:26:42 wes Exp $";
+ 
+#include "gpsee.h"
+#include "system.h"
+
+extern char **environ;
+static struct {
+  const char const *name;
+  const char const *mode;
+} stdioStreams[] = {
+  {"stdin",  "read"},
+  {"stdout", "write"},
+  {"stderr", "write"}
+};
+
+/** Getter system.stdin, .stdout, .stderr; id is actually the file descriptor */
+static JSBool system_stdioGetProperty(JSContext *cx, JSObject *obj, jsval id, jsval *vp)
+{
+  /* This implementation is a little funny in that it calls JS_EvaluateScript() on snprintf()d code.
+   * It probably could be done another way, but this was the quickest, and I don't see any glaring
+   * flaws with this course, other than a slightly bitter taste in my mouth. */
+  static const char codeTemplate[] = "if(!this.hasOwnProperty('file'))this.file=require('fs-base');exports.%s=this.file.openDescriptor(%d,{'%s':true})";
+  char code[sizeof(codeTemplate)+9];
+  const char *propName;
+  int which;
+  JSObject *moduleScope;
+
+  GPSEE_ASSERT(JSVAL_IS_INT(id));
+  which = JSVAL_TO_INT(id);
+  GPSEE_ASSERT(which >= 0 && which <= 2);
+
+  /* Generate Javascript code to run */
+  snprintf(code, sizeof(code), codeTemplate, stdioStreams[which].name, which, stdioStreams[which].mode);
+  //printf("running: %s\n", code);
+  /* We guarantee that "obj" is only going to be our "exports" property, therefore
+   * its parent object is the system module scope.
+   */
+  moduleScope = JS_GetParent(cx, obj);
+  if (!JS_EvaluateScript(cx, moduleScope, code, strlen(code), __FILE__, __LINE__, vp))
+    return JS_FALSE;
+  return JS_TRUE;
+}
+
+/** Setter system.stdin, .stdout, .stderr; id is actually the file descriptor */
+static JSBool system_stdioSetProperty(JSContext *cx, JSObject *obj, jsval id, jsval *vp)
+{
+  int which;
+  const char *propName;
+
+  GPSEE_ASSERT(JSVAL_IS_INT(id));
+  which = JSVAL_TO_INT(id);
+  GPSEE_ASSERT(which >= 0 && which <= 2);
+
+  propName = stdioStreams[which].name;
+  if (!JS_DeleteProperty(cx, obj, propName))
+    return JS_FALSE;
+
+  if (!JS_SetProperty(cx, obj, propName, vp))
+    return JS_FALSE;
+
+  return JS_TRUE;
+}
+
+static JSBool system_platform_getter(JSContext *cx, JSObject *obj, jsval id, jsval *vp)
+{
+  *vp = STRING_TO_JSVAL(JS_InternString(cx, GPSEE_GLOBAL_NAMESPACE_NAME " " GPSEE_CURRENT_VERSION_STRING));
+
+  return JS_TRUE;
+}
+
+static JSBool system_global_getter(JSContext *cx, JSObject *obj, jsval id, jsval *vp)
+{
+  gpsee_interpreter_t 	*jsi = JS_GetRuntimePrivate(JS_GetRuntime(cx));
+
+  *vp = OBJECT_TO_JSVAL(jsi->globalObj);
+
+  return JS_TRUE;
+}
+
+static JSBool system_print(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
+{
+  static const char 	js[]="require('system').stdout.writeln";
+  jsval			v;
+
+  if (JS_EvaluateScript(cx, obj, js, sizeof(js) - 1, __FILE__, __LINE__, &v) == JS_FALSE)
+    return JS_FALSE;
+
+  return JS_CallFunctionValue(cx, obj, v, argc, argv, rval);
+}
+
+const char *system_InitModule(JSContext *cx, JSObject *module)
+{
+  gpsee_interpreter_t 	*jsi;
+  static JSPropertySpec properties[] = 
+  {
+    { "stdin",  	0, JSPROP_ENUMERATE, system_stdioGetProperty, 	system_stdioSetProperty },
+    { "stdout", 	1, JSPROP_ENUMERATE, system_stdioGetProperty, 	system_stdioSetProperty },
+    { "stderr", 	2, JSPROP_ENUMERATE, system_stdioGetProperty, 	system_stdioSetProperty },
+    { "platform",	0, JSPROP_ENUMERATE, system_platform_getter,  	JS_PropertyStub },
+    { "global",		0, JSPROP_ENUMERATE, system_global_getter,	JS_PropertyStub },
+    { NULL, 0, 0, NULL, NULL }
+  };
+
+  static JSFunctionSpec	methods[] = 
+  {
+    { "print",			system_print,			0, 0, 0 },
+    { NULL,			NULL,				0, 0, 0 },
+  };
+
+  if (!JS_DefineProperties(cx, module, properties))
+    return NULL;
+
+  if (JS_DefineFunctions(cx, module, methods) != JS_TRUE)
+    return NULL;
+
+  jsi = (gpsee_interpreter_t*)JS_GetRuntimePrivate(JS_GetRuntime(cx));
+
+  if (!system_InitEnv(cx, module))
+    return NULL;
+
+  if (!gpsee_createJSArray_fromVector(cx, module, "args", jsi->script_argv))
+    return NULL;
+
+  return MODULE_ID; 
+}
+
+JSBool system_FiniModule(JSContext *cx, JSObject *moduleObject)
+{
+  return JS_TRUE;
+}
+
