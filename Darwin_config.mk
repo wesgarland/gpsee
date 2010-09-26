87032524da1a825b011e7fb8cd940bdd2ddaf9f7
Wes Garland <wes@page.ca>
2010-09-26 15:03:34 -0400

    Build system: Fixed staged build system so that solib paths are correct

--- Darwin_config.mk
+++ Darwin_config.mk
@@ -37,7 +37,7 @@ DEFAULT_GPSEE_PREFIX_DIR = /opt/local/gpsee
 LEADING_CPPFLAGS	+= -D_GNU_SOURCE
 SOLIB_EXT	 	 = dylib
 LDFLAGS_SOLIB_DIRS	 = $(foreach DIR, $(SOLIB_DIRS), -L$(DIR))
-LINKER		 	 = $(GCC) -dynamiclib $(JSAPI_LIBS) -undefined dynamic_lookup
+LINKER		 	 = $(GCC) -dynamiclib -undefined dynamic_lookup
 CC			 = $(GCC)
 
 GPSEE_C_DEFINES         += HAVE_NDBM
