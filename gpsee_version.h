5124f25c047883b0ca7e70333654dc9c96c8d60e
Wes Garland <wes@page.ca>
2010-09-26 23:06:45 -0400

    Updated version and fixed depends to notice change

--- gpsee_version.h
+++ gpsee_version.h
@@ -3,5 +3,5 @@
 #define GPSEE_MAJOR_VERSION_NUMBER              0
 #define GPSEE_MINOR_VERSION_NUMBER              2
 #define GPSEE_MICRO_VERSION_NUMBER              0
-#define GPSEE_PATCHLEVEL                        "pre3"
+#define GPSEE_PATCHLEVEL                        "rc1"
 #define GPSEE_CURRENT_VERSION_STRING		_xstr(GPSEE_MAJOR_VERSION_NUMBER) "." _xstr(GPSEE_MINOR_VERSION_NUMBER) "." _xstr(GPSEE_MICRO_VERSION_NUMBER) "-" GPSEE_PATCHLEVEL
