97c7e2da5c7ae15405ac62a143e17201afd01c04
Wes Garland <wes@page.ca>
2011-04-04 14:17:57 -0400

    Update pairodice module to build with modern GPSEE, improve comments, and use best-practices coding style

--- modules/pairodice/pairodice.c
+++ modules/pairodice/pairodice.c
@@ -37,21 +37,35 @@
  *  @file	pairodice_module.c	A GPSEE module implementing a native class for simulating die-pair rolling.
  *					This is an exemplar module, provided to show how trivial native modules
  *					are implemented.
+ *
  *  @author	Wes Garland
  *              PageMail, Inc.
  *		wes@page.ca
  *  @date	Jan 2008
- *  @version	$Id: pairodice.c,v 1.2 2010/03/06 18:17:14 wes Exp $
+ *  @version	$Id: pairodice.c,v 1.3 2011/04/04 18:17:52 wes Exp $
+ *
+ *  @note	To use this module, you must remove it from the IGNORE_MODULES list in the
+ *		main GPSEE Makefile, then re-build and re-install.
  */
 
-static const char __attribute__((unused)) rcsid[]="$Id: pairodice.c,v 1.2 2010/03/06 18:17:14 wes Exp $";
-#include "gpsee.h"
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <time.h>
+ /*
+  * Sample Program:
+  *
+  * 	var PairODice = require("pairodice").PairODice;
+  * 	var dice = new PairODice();
+  *
+  * 	print("First  roll: " + dice.roll());
+  * 	print("Second roll: " + dice.roll());
+  * 	print("Third  roll: " + dice.roll());
+  */
 
-#define MODULE_ID PROJECT_MODULE_ID ".pairodice"
+static const char __attribute__((unused)) rcsid[]="$Id: pairodice.c,v 1.3 2011/04/04 18:17:52 wes Exp $";
+
+#include "gpsee.h"
+
+#define MODULE_ID GPSEE_GLOBAL_NAMESPACE_NAME ".pairodice"
+#define CLASS_ID MODULE_ID ".PairODice"
+static JSClass *pod_clasp;
 
 /** Private C data used by instances of the PairODice class */
 typedef struct
@@ -75,7 +89,6 @@ static JSBool PairODice(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, j
 {
   dice_private_t	*private;
 
-  /* PairODice() called as function. */   
   if (JS_IsConstructing(cx) != JS_TRUE)
     return gpsee_throw(cx, MODULE_ID ".PairODice.constructor.notFunction: Cannot call constructor as a function!");
 
@@ -86,17 +99,15 @@ static JSBool PairODice(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, j
 
   private = JS_malloc(cx, sizeof(*private));
   if (!private)
-  {
-    JS_ReportOutOfMemory(cx);
     return JS_FALSE;
-  }
 
   memset(private, 0, sizeof(*private));
 
-  /* Use the private member to maintain separate 
-   * random number seeds for each pair of dice
+  /* Use the private member to maintain separate random number seeds for each pair of dice.
+   * We seed the random number generator with entroy derived from the system memory
+   * allocator, clock, and process ID.
    */
-  private->seed = (time(NULL) & getpid()) % RAND_MAX;
+  private->seed = (((size_t)private >> 3) + time(NULL) + getpid()) % RAND_MAX;
   (void)rand_r(&private->seed);
   JS_SetPrivate(cx, obj, private);
 
@@ -138,10 +149,11 @@ static void PairODice_Finalize(JSContext *cx, JSObject *obj)
  */
 static JSBool pod_roll(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
 {
-  dice_private_t 	*private = JS_GetPrivate(cx, obj);
+  dice_private_t 	*private = JS_GetInstancePrivate(cx, obj, pod_clasp, NULL);
   int			dieOne, dieTwo;
 
-  GPSEE_ASSERT(private != NULL);
+  if (!private)
+    return gpsee_throw(cx, CLASS_ID ".roll.instanceof: cannot apply method on this object");
 
   dieOne = (rand_r(&private->seed) % 6) + 1;
   dieTwo = (rand_r(&private->seed) % 6) + 1;
@@ -166,17 +178,13 @@ static JSBool pod_roll(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, js
  */
 static JSBool pod_lastRoll_getter(JSContext *cx, JSObject *obj, jsval id, jsval *vp)
 {
-  dice_private_t 	*private = JS_GetPrivate(cx, obj);
+  dice_private_t 	*private = JS_GetInstancePrivate(cx, obj, pod_clasp, NULL);
+
+  if (!private)
+    return gpsee_throw(cx, CLASS_ID ".roll.instanceof: cannot apply method on this object");
 
   *vp = INT_TO_JSVAL(private->lastRoll);
 
-  return JS_TRUE;
-}
-
-/** Release all resources allocated by the PairODice class.
- */
-static JSBool PairODice_FiniClass(JSContext *cx, JSObject *proto)
-{ 
   return JS_TRUE;
 }
  
@@ -190,7 +198,6 @@ static JSBool PairODice_FiniClass(JSContext *cx, JSObject *proto)
  */
 static JSObject *PairODice_InitClass(JSContext *cx, JSObject *obj)
 {
-  /** Description of this class: */
   static JSClass pod_class = 
   {
     GPSEE_CLASS_NAME(PairODice),	/**< its name is PairODice */
@@ -220,7 +227,6 @@ static JSObject *PairODice_InitClass(JSContext *cx, JSObject *obj)
     { NULL, 0, 0, NULL, NULL }
   };
 
-
   JSObject *proto = 
       JS_InitClass(cx, 			/* JS context from which to derive runtime information */
 		   obj, 		/* Object to use for initializing class (constructor arg?) */
@@ -233,12 +239,14 @@ static JSObject *PairODice_InitClass(JSContext *cx, JSObject *obj)
 		   NULL,		/* static_ps - static props struct for constructor */
 		   NULL); 		/* static_fs - static funcs struct for constructor (methods like Math.Abs()) */
 
+  pod_clasp = &pod_class;
   return proto;
 }
 
+/** Initialize the module, adding the PairODice export */
 const char *pairodice_InitModule(JSContext *cx, JSObject *moduleObject)
 {
-  JSObject *prototype = JS_InitClass(cx, moduleObject);
+  JSObject *prototype = PairODice_InitClass(cx, moduleObject);
 
   if (!prototype)
     return NULL;
@@ -246,6 +254,10 @@ const char *pairodice_InitModule(JSContext *cx, JSObject *moduleObject)
   return MODULE_ID;
 }
 
+/**
+ *  Finalize Module.  If we return JS_TRUE, that means this module has no (important) internal state, 
+ *  which means that it is safe to unload the module if it was loaded as a dynamic shared object (DSO).
+ */
 JSBool pairodice_FiniModule(JSContext *cx, JSObject *moduleObject)
 {
   return JS_TRUE;	/* Safe to unload this if it was running as DSO module */
