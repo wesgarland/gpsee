350708fe2e1411f5b7497f76e3cb6477d01fd2f4
Wes Garland <wes@page.ca>
2011-10-10 08:55:44 -0400

    Test suite - added modules/batteries

---
+++ tests/modules/batteries/program.js
@@ -0,0 +1,15 @@
+require("binary");
+require("cgi");
+require("curses");
+require("fs-base");
+require("gffi");
+require("gpsee");
+require("net");
+require("shellalike");
+require("signal");
+require("syslog");
+require("system");
+require("util"); 
+require("vm"); 
+
+require("fs-base").openRaw("/dev/null", { read: true });
