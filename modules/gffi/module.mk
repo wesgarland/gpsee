2dcfe8dc7420c158b24ba60bc353cc584078b28d
Wes Garland <wes@page.ca>
2012-04-18 16:52:43 -0400

    GFFI build - added guard/warning for flip-flopping existence of integer defines from compiler itself

--- modules/gffi/module.mk
+++ modules/gffi/module.mk
@@ -230,8 +230,12 @@ TMS_EXPR=([A-Za-z0-9_()~!+-][\" A-Za-z0-9_()~!^&|<>,+-]*)
 	| $(SED) \
 		-e 's/  */ /g' \
 		-e 's/^\(#define \)\([^ ][^ ]*\)\(.*\)/\
+		#ifdef \2\
 		printf("haveInt(\2,"); \
-	        printf(((\2 < 0) ? "%lld,1," GPSEE_SIZET_FMT ")\\n":"%llu,0," GPSEE_SIZET_FMT ")\\n"),(long long)(\2),sizeof(\2));/' \
+	        printf(((\2 < 0) ? "%lld,1," GPSEE_SIZET_FMT ")\\n":"%llu,0," GPSEE_SIZET_FMT ")\\n"),(long long)(\2),sizeof(\2));\
+		#else\
+		#warning "Missing integer define \2"\
+		#endif/' \
 	>> $@
 	@echo " - Floating-point Expression"
 	@$(EGREP) '$(START)$(FLOAT_EXPR)$$' $*_defs.dmp \
